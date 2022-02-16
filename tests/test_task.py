794859e44759885efeb900a1b9cc4e5db1dc4ac2
Stéphane Caron <stephane.caron@normalesup.org>
2022-02-16 18:24:43 +0100

    Test task base class

---
+++ tests/test_task.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2022 Stéphane Caron.
+#
+# This file is part of Pink.
+#
+# Pink is free software: you can redistribute it and/or modify it under the
+# terms of the GNU Lesser General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option) any
+# later version.
+#
+# Pink is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with Pink. If not, see <http://www.gnu.org/licenses/>.
+
+"""
+Test base class for kinematic tasks.
+"""
+
+import unittest
+
+from pink.tasks import Task
+
+from .models import build_jvrc_model
+
+
+class TestTask(unittest.TestCase):
+    def setUp(self):
+        """
+        Prepare test fixture.
+        """
+        self.robot = build_jvrc_model()
+        self.assertIsNotNone(self.robot)
+
+    def test_task_not_implemented(self):
+        """
+        Raise an error when the robot body is not found.
+        """
+        task = Task()
+        self.assertIsNotNone(task)
+        with self.assertRaises(NotImplementedError):
+            task.compute_task_dynamics(self.robot)
+        with self.assertRaises(NotImplementedError):
+            task.compute_qp_objective(self.robot)
+
+
+if __name__ == "__main__":
+    unittest.main()
