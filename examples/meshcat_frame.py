cd0aa4f8aad6b7bd42a297ab86c4d57e657bdbc0
Stéphane Caron <stephane.caron@normalesup.org>
2022-03-17 17:29:18 +0100

    Remember to move MeshCat utility functions
    Someday all these TODO's will be cleaned up. Someday. ;p

--- examples/meshcat_frame.py
+++ examples/meshcat_frame.py
@@ -18,6 +18,10 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with Pink. If not, see <http://www.gnu.org/licenses/>.
 
+"""
+TODO(scaron): move these utility functions somewhere logical.
+"""
+
 import meshcat
 import numpy as np
 
