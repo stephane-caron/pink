b482f131f9ae5bbec12f0af616434d015c55580f
Stéphane Caron <stephane.caron@normalesup.org>
2022-03-17 10:50:53 +0100

    WIP: task exceptions

---
+++ pink/tasks/exceptions.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2022 Stéphane Caron.
+#
+# This file is part of Pink.
+#
+# Pink is free software: you can redistribute it and/or modify it under the
+# terms of the GNU Lesser General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option) any
+# later version.
+#
+# Pink is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with Pink. If not, see <http://www.gnu.org/licenses/>.
+
+"""
+Exceptions raised by tasks.
+"""
+
+
+class TargetNotSet(Exception):
+
+    """
+    Exception raised when attempting to compute a task with an unset target.
+    """
+
+    pass
