7a87be2fa00ac98aa400b988cbadb8200fb6bb6c
Stéphane Caron <stephane.caron@normalesup.org>
2022-04-01 20:47:39 +0200

    Add Coveralls to GitHub Actions

---
+++ pink/utils.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Copyright 2022 Stéphane Caron
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+Utility functions.
+"""
+
+import numpy as np
+import pinocchio as pin
+
+
+def custom_configuration_vector(robot, **kwargs) -> np.ndarray:
+    q = pin.neutral(robot.model)
+    for joint_name, joint_value in kwargs.items():
+        joint_id = robot.model.getJointId(joint_name)
+        joint = robot.model.joints[joint_id]
+        q[joint.idx_q] = joint_value
+    return q
