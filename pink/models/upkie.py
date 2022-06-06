5bd5f99dad18481c060cbd0b15bc98b9ea994453
Stéphane Caron <stephane.caron@normalesup.org>
2022-06-06 10:07:29 +0200

    [minor] Link to Upkie's project page on Hackaday.io

--- pink/models/upkie.py
+++ pink/models/upkie.py
@@ -19,7 +19,7 @@
 `Upkie`_ is a homemade wheeled biped robot that can balance, crouch and turn
 around. It proudly stands on broomsticks and 3D printed parts (^_^)
 
-.. _Upkie: https://www.youtube.com/watch?v=8b36XcCgh7s
+.. _Upkie: https://hackaday.io/project/185729-upkie-wheeled-biped-robot
 """
 
 import os
