9db76ec037e993ba77cffa49c98db74506df0edd
Stéphane Caron <stephane.caron@normalesup.org>
2022-09-09 09:58:21 +0200

    Convenience function to load a robot description

--- pink/utils.py
+++ pink/utils.py
@@ -19,10 +19,32 @@
 Utility functions.
 """
 
-from typing import List
+import os.path
+from importlib import import_module  # type: ignore
+from typing import List, Union
 
 import numpy as np
 import pinocchio as pin
+
+try:
+    import robot_descriptions
+except ImportError:
+    robot_descriptions = None
+
+
+PinocchioJoint = Union[
+    pin.JointModelRX,
+    pin.JointModelRY,
+    pin.JointModelRZ,
+    pin.JointModelPX,
+    pin.JointModelPY,
+    pin.JointModelPZ,
+    pin.JointModelFreeFlyer,
+    pin.JointModelSpherical,
+    pin.JointModelSphericalZYX,
+    pin.JointModelPlanar,
+    pin.JointModelTranslation,
+]
 
 
 def custom_configuration_vector(robot: pin.Model, **kwargs) -> np.ndarray:
@@ -47,7 +69,7 @@ def custom_configuration_vector(robot: pin.Model, **kwargs) -> np.ndarray:
 def load_urdf(
     urdf_path: str,
     package_dirs: List[str] = [],
-    free_flyer: bool = True,
+    root_joint_type: PinocchioJoint = pin.JointModelFreeFlyer,
 ) -> pin.RobotWrapper:
     """
     Load the URDF of a robot description.
@@ -55,12 +77,51 @@ def load_urdf(
     Args:
         urdf_path: Path to the URDF file.
         package_dirs: List of directories Pinocchio can use as guesses to
-            replace relative and ``package://`` paths from the URDF.
-        free_flyer: Add a free flyer (a.k.a. floating base) as the first joint
-            of the kinematic chain.
+            expand relative and ``package://`` paths in the URDF.
+        root_joint_type: Type of the first joint of the kinematic chain,
+            typically a free flyer (a.k.a. floating base) for mobile robots.
+
+    Returns:
+        Robot models for Pinocchio.
     """
     return pin.RobotWrapper.BuildFromURDF(
         filename=urdf_path,
         package_dirs=package_dirs,
-        root_joint=pin.JointModelFreeFlyer() if free_flyer else None,
+        root_joint=root_joint_type(),
     )
+
+
+def load_robot_description(
+    description_name: str,
+    root_joint_type: PinocchioJoint = pin.JointModelFreeFlyer,
+) -> pin.RobotWrapper:
+    """
+    Load a robot description from `robot_descriptions
+    <https://pypi.org/project/robot_descriptions/>`__.
+
+    Args:
+        description_name: Name of the robot description.
+        root_joint_type: Type of the first joint of the kinematic chain,
+            typically a free flyer (a.k.a. floating base) for mobile robots.
+
+    Returns:
+        Robot models for Pinocchio.
+    """
+    if robot_descriptions is None:
+        raise ImportError(
+            "robot_descriptions not found, "
+            "try ``pip install robot_descriptions``"
+        )
+    module = import_module(f"robot_descriptions.{description_name}")
+    robot = pin.RobotWrapper.BuildFromURDF(
+        filename=module.URDF_PATH,
+        package_dirs=[
+            module.MESHES_PATH,
+            module.PACKAGE_PATH,
+            module.REPOSITORY_PATH,
+            os.path.dirname(module.PACKAGE_PATH),
+            os.path.dirname(module.REPOSITORY_PATH),
+        ],
+        root_joint=root_joint_type(),
+    )
+    return robot
