cf059f0c19e742eb2bdc18465cff88810e88cac9
Stéphane Caron <stephane.caron@normalesup.org>
2022-09-22 14:34:27 +0200

    WIP: Visualizer helper function

---
+++ pink/visualization.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Copyright 2022 Stéphane Caron
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+Visualization helpers.
+"""
+
+import pinocchio as pin
+
+
+def start_meshcat_visualizer(robot: pin.RobotWrapper):
+    """
+    Open a MeshCat visualizer in a web browser.
+
+    Args:
+        robot: Robot wrapper with its initial data.
+
+    Returns:
+        MeshCat visualizer.
+    """
+    viz = pin.visualize.MeshcatVisualizer(
+        robot.model, robot.collision_model, robot.visual_model
+    )
+    robot.setVisualizer(viz, init=False)
+    viz.initViewer(open=True)
+    viz.loadViewerModel()
+    return viz
