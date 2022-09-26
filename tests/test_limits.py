6ee874e32f710b30fea352ab1eb0c56ac83be7c9
Stéphane Caron <stephane.caron@normalesup.org>
2022-09-26 14:54:09 +0200

    Fix unit tests for configuration limits

--- tests/test_limits.py
+++ tests/test_limits.py
@@ -21,8 +21,8 @@ Test velocity and actuated-joint limits.
 
 import unittest
 
-import pinocchio as pin
 import numpy as np
+import pinocchio as pin
 from robot_descriptions.loaders.pinocchio import load_robot_description
 
 from pink import apply_configuration
@@ -31,35 +31,33 @@ from pink.limits import compute_velocity_limits
 
 class TestLimits(unittest.TestCase):
     def setUp(self):
-        robot = load_robot_description(
+        self.robot = load_robot_description(
             "upkie_description", root_joint=pin.JointModelFreeFlyer()
         )
-        configuration = apply_configuration(robot, robot.q0)
-        self.dt = 1e-3
-        self.configuration = configuration
-        self.robot = robot
+        self.dt = 1e-3  # [s]
 
     def test_forward_velocity_limit(self):
         """
         When we are far away from configuration limits, the velocity limit is
         simply the configuration-agnostic one from the robot.
         """
-        v_max, v_min = compute_velocity_limits(self.configuration, self.dt)
-        velocity_limit = self.configuration.model.velocityLimit
-        self.assertTrue(np.allclose(v_max, +velocity_limit))
-        self.assertTrue(np.allclose(v_min, -velocity_limit))
+        configuration = apply_configuration(self.robot, self.robot.q0)
+        v_max, v_min = compute_velocity_limits(configuration, self.dt)
+        model_v_lim = configuration.model.velocityLimit
+        self.assertTrue(np.allclose(v_max, +model_v_lim))
+        self.assertTrue(np.allclose(v_min, -model_v_lim))
 
     def test_configuration_limit_repulsion(self):
         """
         Velocities are scaled down when close to a configuration limit.
         """
-        q = self.configuration.q
-        slack_vel = 5.5e-4  # [rad] / [s]
-        self.robot.model.upperPositionLimit = q + slack_vel * self.dt
-        v_max, v_min = compute_velocity_limits(
-            self.configuration, self.dt, config_limit_gain=0.5
+        configuration = apply_configuration(self.robot, self.robot.q0)
+        slack_vel = 5.5e-4 * configuration.tangent.ones
+        self.robot.model.upperPositionLimit = configuration.integrate(
+            slack_vel, self.dt
         )
-        default_limit = self.robot.model.velocityLimit
-        self.assertTrue(np.allclose(v_max[:6], default_limit[:6]))
-        self.assertTrue(np.all(v_max[6:] < default_limit[6:]))
-        self.assertTrue(np.all(v_max[6:] < slack_vel))
+        v_max, v_min = compute_velocity_limits(
+            configuration, self.dt, config_limit_gain=0.5
+        )
+        self.assertTrue(np.all(v_max < self.robot.model.velocityLimit))
+        self.assertTrue(np.all(v_max < slack_vel))
