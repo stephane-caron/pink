2d94db6c4fe8caf95a81b4ceeff49afe04c59f4e
Stéphane Caron <stephane.caron@normalesup.org>
2022-12-01 15:25:35 +0100

    Expose build_ik

--- pink/__init__.py
+++ pink/__init__.py
@@ -24,7 +24,7 @@ from .configuration import (
     apply_configuration,
     assume_configuration,
 )
-from .solve_ik import solve_ik
+from .solve_ik import build_ik, solve_ik
 
 __version__ = "0.6.0"
 
@@ -32,5 +32,6 @@ __all__ = [
     "Configuration",
     "apply_configuration",
     "assume_configuration",
+    "build_ik",
     "solve_ik",
 ]
