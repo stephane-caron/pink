c026ed29d94e159598df896e7e8e0d32e68212de
Stéphane Caron <stephane.caron@normalesup.org>
2023-01-23 21:42:19 +0100

    Document spatial and body minus operators

--- pink/tasks/utils.py
+++ pink/tasks/utils.py
@@ -21,88 +21,83 @@ import numpy as np
 import pinocchio as pin
 
 
-def box_minus(plus_transform: pin.SE3, minus_transform: pin.SE3) -> np.ndarray:
-    r"""Compute the box minus between two transforms.
+def spatial_minus(Y: pin.SE3, X: pin.SE3) -> np.ndarray:
+    r"""Compute the left minus :math:`Y \ominus_0 X`.
 
-    The box minus operator is defined by:
+    The left minus operator is defined by:
 
     .. math::
 
-        T_1 \boxminus T_2 = \log(T_1 \cdot T_2^{-1})
+        Y \ominus_s X = \log(Y \cdot X^{-1})
 
     This operator allows us to think about orientation "differences" as
-    similarly as possible to position differences, but mind the frames! Its
-    formula has two use cases, depending on whether the common frame :math:`C`
-    between the two transforms is their source or their target.
-
-    When the common frame is the target, denoting by :math:`T_{CP}` the
-    transform from frame :math:`P` (source) to frame :math:`C` (target), the
-    resulting twist is expressed in the target frame:
+    similarly as possible to position differences, but mind the frames! If we
+    denote by :math:`Y = T_{0y}` and :math:`X = T_{0x}` the two transforms,
+    from respectively frames :math:`y` and :math:`x` to the inertial frame
+    :math:`0`, the twist resulting from a left minus is expressed in the
+    inertial frame:
 
     .. math::
 
-        {}_C \xi_{CM} = T_{CP} \boxminus T_{CM}
+        {}_0 \xi_{0x} = Y \ominus_0 X = \log(T_{0y} T_{x0})
 
-    When the common frame is the source frame, denoting by :math:`T_{MC}` the
-    transform from frame :math:`C` (source) to frame :math:`M` (target), the
-    resulting twist is expressed in the target frame of the transform on the
-    right-hand side of the operator:
-
-    .. math::
-
-        -{}_M \xi_{M} = T_{PC} \boxminus T_{MC}
+    A twist like :math:`{}_0 \xi_{0x}` in the inertial frame is called a
+    spatial motion vector.
 
     Args:
-        plus_transform: Transform :math:`T_1` on the left-hand side of the box
-            minus operator.
-        minus_transform: Transform :math:`T_2` on the right-hand side of the
-            box minus operator.
+        Y: Transform :math:`Y = T_{0y}` on the left-hand side of the operator.
+        X: Transform :math:`X = T_{0x}` on the right-hand side of the operator.
 
     Returns:
-        In the first case :math:`T_{CP} \boxminus T_{CM}`, the outcome is a
-        spatial twist :math:`{}_C \xi_{CM}` expressed in the common frame
-        :math:`C`.
+        Spatial motion vector resulting from the difference :math:`\ominus_0`
+        between :math:`Y` and :math:`X`.
 
-        In the second case :math:`T_{PC} \boxminus T_{MC}`, the outcome is a
-        body twist :math:`-{}_M \xi_{CM}` (mind the unitary minus).
-
-    Note:
-        Prefer using :func:`pink.tasks.utils.body_box_minus` to calling this
-        function in the second use case :math:`T_{PC} \boxminus T_{MC}`.
+    See also:
+        The `micro Lie theory <https://arxiv.org/abs/1812.01537>`_ describes
+        the difference between the left and right minus operators.
     """
-    diff_array = plus_transform.act(minus_transform.inverse())
-    twist: np.ndarray = pin.log(diff_array).vector
-    return twist
+    spatial_twist: np.ndarray = pin.log(Y.act(X.inverse())).vector
+    return spatial_twist
 
 
-def body_box_minus(
-    transform_plus_to_common: pin.SE3,
-    transform_minus_to_common: pin.SE3,
-) -> np.ndarray:
-    r"""Compute the body variant of the box minus.
+def body_minus(Y: pin.SE3, X: pin.SE3) -> np.ndarray:
+    r"""Compute the right minus :math:`Y \ominus X`.
 
-    The body error between two poses :math:`P` (plus) and :math:`M` (minus),
-    represented by their transforms :math:`T_{CP}` and :math:`T_{CM}` to a
-    common frame :math:`C`, is defined in Pink by:
+    The right minus operator is defined by:
 
     .. math::
 
-        {}_M \xi_{CM} = -(T_{PC} \boxminus T_{MC})
-                       = -(T_{CP}^{-1} \boxminus T_{CM}^{-1})
+        Y \ominus X = \log(X^{-1} \cdot Y)
+
+    This operator allows us to think about orientation "differences" as
+    similarly as possible to position differences, but mind the frames! If we
+    denote by :math:`Y = T_{0y}` and :math:`X = T_{0x}` the two transforms,
+    from respectively frames :math:`y` and :math:`x` to the inertial frame
+    :math:`0`, the twist resulting from a right minus is expressed in the
+    local frame :math:`x`:
+
+    .. math::
+
+        {}_x \xi_{0x} = Y \ominus X = \log(T_{x0} T{0y}) = \log(T_{xy})
+
+    A twist like :math:`{}_x \xi_{0x}` in the local frame :math:`x` is called a
+    body motion vector.
 
     Args:
-        transform_plus_to_common: Transform :math:`T_{CP}` from the "plus"
-            frame to the common frame.
-        transform_minus_to_common: Transform :math:`T_{CM}` from the "minus"
-            frame to the common frame.
+        Y: Transform :math:`Y = T_{0y}` on the left-hand side of the operator.
+        X: Transform :math:`X = T_{0x}` on the right-hand side of the operator.
 
     Returns:
-        Body twist :math:`{}_M \xi_{CM}`. If :math:`C` is inertial (and has
-        zero velocity) we can simply write it :math:`{}_M \xi_M` like other
-        `body screws
-        <https://scaron.info/robotics/screw-theory.html#body-screws>`_.
+        Body motion vector resulting from the difference :math:`\ominus_0`
+        between :math:`Y` and :math:`X`.
+
+    See also:
+        `Body motion vector
+        <https://scaron.info/robotics/screw-theory.html#body-screws>`_ are also
+        known as body screws in screw theory.
+
+        The `micro Lie theory <https://arxiv.org/abs/1812.01537>`_ describes
+        the difference between the left and right minus operators.
     """
-    return box_minus(
-        transform_plus_to_common.inverse(),
-        transform_minus_to_common.inverse(),
-    )
+    body_twist: np.ndarray = pin.log(X.actInv(Y)).vector
+    return body_twist
