6e72db0c823554b70be42ef1249b1d03d9061511
Stéphane Caron <stephane.caron@normalesup.org>
2023-02-14 16:44:31 +0100

    Move configuration limit gain to ctor

--- pink/limits/configuration_limit.py
+++ pink/limits/configuration_limit.py
@@ -30,21 +30,33 @@ class ConfigurationLimit(Limit):
     """Subspace of the tangent space restricted to joints with position limits.
 
     Attributes:
+        config_limit_gain: gain between 0 and 1 to steer away from
+            configuration limits. It is described in "Real-time prioritized
+            kinematic control under inequality constraints for redundant
+            manipulators" (Kanoun, 2012). More details in `this writeup
+            <https://scaron.info/teaching/inverse-kinematics.html>`__.
         model: Robot model the limit applies to.
         joints: Joints with configuration limits.
+        projection_matrix: Projection from tangent space to subspace with
+            configuration-limited joints.
     """
 
+    config_limit_gain: float
     indices: np.ndarray
     model: pin.Model
     joints: list
-    __projection_matrix: Optional[np.ndarray]
+    projection_matrix: Optional[np.ndarray]
 
-    def __init__(self, model: pin.Model):
+    def __init__(self, model: pin.Model, config_limit_gain: float = 0.5):
         """Initialize bounded tangent of a model.
 
         Args:
             model: robot model.
+            config_limit_gain: gain between 0 and 1 to steer away from
+                configuration limits. See the full class documentation.
         """
+        assert 0.0 < config_limit_gain <= 1.0
+
         has_configuration_limit = np.logical_and(
             model.upperPositionLimit < 1e20,
             model.upperPositionLimit > model.lowerPositionLimit + 1e-10,
@@ -68,16 +80,16 @@ class ConfigurationLimit(Limit):
         dim = len(indices)
         projection_matrix = np.eye(model.nv)[indices] if dim > 0 else None
 
+        self.config_limit_gain = config_limit_gain
         self.indices = indices
         self.joints = joints
         self.model = model
-        self.__projection_matrix = projection_matrix
+        self.projection_matrix = projection_matrix
 
     def compute_qp_inequalities(
         self,
         q: np.ndarray,
         dt: float,
-        config_limit_gain: float = 0.5,
     ) -> Optional[Tuple[np.ndarray, np.ndarray]]:
         r"""Compute the configuration-dependent velocity limits.
 
@@ -95,17 +107,11 @@ class ConfigurationLimit(Limit):
         Args:
             q: Robot configuration.
             dt: Integration timestep in [s].
-            config_limit_gain: gain between 0 and 1 to steer away from
-                configuration limits. It is described in "Real-time prioritized
-                kinematic control under inequality constraints for redundant
-                manipulators" (Kanoun, 2012). More details in `this writeup
-                <https://scaron.info/teaching/inverse-kinematics.html>`__.
 
         Returns:
             Pair :math:`(G, h)` representing the inequality constraint as
             :math:`G \Delta q \leq h`, or ``None`` if there is no limit.
         """
-        assert 0.0 < config_limit_gain <= 1.0
         if not self.joints:
             return None
 
@@ -115,8 +121,8 @@ class ConfigurationLimit(Limit):
         Delta_q_min = pin.difference(
             self.model, q, self.model.lowerPositionLimit
         )
-        Delta_q_max = config_limit_gain * Delta_q_max[self.indices]
-        Delta_q_min = config_limit_gain * Delta_q_min[self.indices]
-        G = np.vstack([self.__projection_matrix, -self.__projection_matrix])
-        h = np.hstack([Delta_q_max, -Delta_q_min])
+        p_max = self.config_limit_gain * Delta_q_max[self.indices]
+        p_min = self.config_limit_gain * Delta_q_min[self.indices]
+        G = np.vstack([self.projection_matrix, -self.projection_matrix])
+        h = np.hstack([p_max, -p_min])
         return G, h
