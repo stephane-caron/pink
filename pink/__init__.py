12238e308f15c58ea0d091835092936587d51172
Stéphane Caron <stephane.caron@normalesup.org>
2024-01-30 16:15:19 +0100

    Re-export tasks from top-level module

--- pink/__init__.py
+++ pink/__init__.py
@@ -3,18 +3,29 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 # Copyright 2022 Stéphane Caron
+# Copyright 2024 Inria
 
 """Inverse kinematics for articulated robot models, based on Pinocchio."""
 
 from .configuration import Configuration
 from .solve_ik import build_ik, solve_ik
-from .tasks import Task
+from .tasks import (
+    FrameTask,
+    JointCouplingTask,
+    LinearHolonomicTask,
+    PostureTask,
+    Task,
+)
 from .utils import custom_configuration_vector
 
 __version__ = "1.1.0"
 
 __all__ = [
     "Configuration",
+    "FrameTask",
+    "JointCouplingTask",
+    "LinearHolonomicTask",
+    "PostureTask",
     "build_ik",
     "custom_configuration_vector",
     "solve_ik",
