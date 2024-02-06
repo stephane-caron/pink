d5baed2ecd401f8dd358d738b0b89fe904117b6b
Stéphane Caron <stephane.caron@normalesup.org>
2024-02-06 18:30:19 +0100

    Implement damping task from posture task

--- pink/tasks/__init__.py
+++ pink/tasks/__init__.py
@@ -10,10 +10,12 @@ from .exceptions import TargetNotSet, TaskJacobianNotSet
 from .frame_task import FrameTask
 from .joint_coupling_task import JointCouplingTask
 from .linear_holonomic_task import LinearHolonomicTask
+from .damping_task import DampingTask
 from .posture_task import PostureTask
 from .task import Task
 
 __all__ = [
+    "DampingTask",
     "FrameTask",
     "TargetNotSet",
     "TaskJacobianNotSet",
