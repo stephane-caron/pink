1c82bb5f3e9b7ac3d49aa71f7603a8c3c1d4c17d
Ivan Domrachev <domrachev03@mail.ru>
2024-05-03 04:25:21 +0300

    example: ur5 with cbf init

---
+++ examples/cbf/arm_ur5.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# SPDX-License-Identifier: Apache-2.0
+# Copyright 2022 Stéphane Caron
+
+"""Universal Robots UR5 arm tracking a moving target."""
+
+import meshcat_shapes
+import numpy as np
+import qpsolvers
+from loop_rate_limiters import RateLimiter
+
+import pink
+from pink import solve_ik
+from pink.barriers import PositionCBF
+from pink.tasks import FrameTask, PostureTask
+from pink.utils import custom_configuration_vector
+from pink.visualization import start_meshcat_visualizer
+
+try:
+    from robot_descriptions.loaders.pinocchio import load_robot_description
+except ModuleNotFoundError as exc:
+    raise ModuleNotFoundError(
+        "Examples need robot_descriptions, " "try ``pip install robot_descriptions``"
+    ) from exc  # noqa: E501
+
+
+if __name__ == "__main__":
+    robot = load_robot_description("ur5_description", root_joint=None)
+    viz = start_meshcat_visualizer(robot)
+
+    end_effector_task = FrameTask(
+        "ee_link",
+        position_cost=1.0,  # [cost] / [m]
+        orientation_cost=1.0,  # [cost] / [rad]
+        lm_damping=1.0,  # tuned for this setup
+    )
+
+    posture_task = PostureTask(
+        cost=1e-3,  # [cost] / [rad]
+    )
+
+    pos_cbf = PositionCBF("ee_link", max=np.array([10, 0.6, 10]), gain=100.0)
+
+    tasks = [end_effector_task, posture_task]
+
+    q_ref = custom_configuration_vector(
+        robot,
+        shoulder_lift_joint=1.0,
+        shoulder_pan_joint=1.0,
+        elbow_joint=1.0,
+    )
+    configuration = pink.Configuration(robot.model, robot.data, q_ref)
+    for task in tasks:
+        task.set_target_from_configuration(configuration)
+    viz.display(configuration.q)
+
+    viewer = viz.viewer
+    meshcat_shapes.frame(viewer["end_effector_target"], opacity=1.0)
+    # meshcat_shapes.frame(viewer["end_effector"], opacity=1.0)
+
+    # Select QP solver
+    solver = qpsolvers.available_solvers[0]
+    if "osqp" in qpsolvers.available_solvers:
+        solver = "osqp"
+
+    rate = RateLimiter(frequency=200.0)
+    dt = rate.period
+    t = 0.0  # [s]
+    while True:
+        # Update task targets
+        end_effector_target = end_effector_task.transform_target_to_world
+        end_effector_target.translation[1] = 0.5 + 0.2 * np.sin(2.0 * t)
+        end_effector_target.translation[2] = 0.2
+
+        # Update visualization frames
+        viewer["end_effector_target"].set_transform(end_effector_target.np)
+        viewer["end_effector"].set_transform(configuration.get_transform_frame_to_world(end_effector_task.frame).np)
+
+        # Compute velocity and integrate it into next configuration
+        velocity = solve_ik(configuration, tasks, dt, solver=solver, cbfs=[pos_cbf])
+        configuration.integrate_inplace(velocity, dt)
+
+        G_cbf, h_cbf = pos_cbf.compute_qp_inequality(configuration=configuration)
+        print(f"CBF value: {G_cbf@configuration.q - h_cbf}")
+
+        print(configuration.get_transform_frame_to_world("ee_link").translation[1])
+        # Visualize result at fixed FPS
+        viz.display(configuration.q)
+        rate.sleep()
+        t += dt
