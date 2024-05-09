0f94a6c0ce934c73b486f1ad5a45d8d995f7215f
Ivan Domrachev <domrachev03@mail.ru>
2024-05-10 00:42:20 +0300

    feat: full self-collision barrier init

--- pink/barriers/__init__.py
+++ pink/barriers/__init__.py
@@ -2,3 +2,4 @@ from .barrier import CBF
 from .body_spherical_barrier import BodySphericalCBF
 from .configuration_barrier import ConfigurationCBF
 from .position_barrier import PositionCBF
+from .self_collision_barrier import SelfCollisionBarrier
