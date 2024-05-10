1b22d8a3ad34619624cea9875e57896c1e115bb2
Ivan Domrachev <domrachev03@mail.ru>
2024-05-10 18:41:37 +0300

    revert: remove hpp-fcl self-collision code (moved to separate branch)

--- pink/barriers/__init__.py
+++ pink/barriers/__init__.py
@@ -2,4 +2,3 @@ from .barrier import CBF
 from .body_spherical_barrier import BodySphericalCBF
 from .configuration_barrier import ConfigurationCBF
 from .position_barrier import PositionCBF
-from .self_collision_barrier import SelfCollisionBarrier
