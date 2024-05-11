22798c8a23f7f8cde01ba2f67291d7bd28d20f2c
Ivan Domrachev <domrachev03@mail.ru>
2024-05-11 13:55:18 +0300

    Revert "feat: shift point for spherical body collision avoidance barrier"
    This reverts commit 58593542202252c31c23e974468b7f16d518ddd2.

--- pink/barriers/body_spherical_barrier.py
+++ pink/barriers/body_spherical_barrier.py
@@ -5,10 +5,10 @@
 # Copyright 2022 Stéphane Caron
 
 """General description"""
-from typing import Union
+
+from typing import Iterable, Optional, Union
 
 import numpy as np
-import pinocchio as pin
 
 from ..configuration import Configuration
 from .barrier import CBF
@@ -26,10 +26,8 @@ class BodySphericalCBF(CBF):
 
     def __init__(
         self,
-        frames: tuple[str, str],
-        O1: np.ndarray | None = None,
-        O2: np.ndarray | None = None,
-        d_min: float = 0,
+        frame: tuple[str, str],
+        d_min: float = None,
         gain: Union[float, np.ndarray] = 1.0,
         r: float = 3.0,
     ):
@@ -40,35 +38,47 @@ class BodySphericalCBF(CBF):
             # class_k_fn=lambda h: h / (1 + np.linalg.norm(h)),
             r=r,
         )
-        self.frame = frames
-        self.O1 = O1 if O1 is not None else np.zeros(3)
-        self.O2 = O2 if O2 is not None else np.zeros(3)
+        self.frame = frame
         self.d_min = d_min
 
     def compute_barrier(self, configuration: Configuration) -> np.ndarray:
         """..."""
-        pos1_world = configuration.get_transform_frame_to_world(self.frame[0]).translation + self.O1
-        pos2_world = configuration.get_transform_frame_to_world(self.frame[1]).translation + self.O2
+        pos1_world = configuration.get_transform_frame_to_world(
+            self.frame[0]
+        ).translation
+        pos2_world = configuration.get_transform_frame_to_world(
+            self.frame[1]
+        ).translation
 
-        return np.array([np.linalg.norm(pos1_world - pos2_world) ** 2 - self.d_min**2])
+        return np.array(
+            [np.linalg.norm(pos1_world - pos2_world) ** 2 - self.d_min**2]
+        )
 
     def compute_jacobian(self, configuration: Configuration) -> np.ndarray:
         """..."""
-        pos1_world = configuration.get_transform_frame_to_world(self.frame[0]).translation + self.O1
-        pos2_world = configuration.get_transform_frame_to_world(self.frame[1]).translation + self.O2
+        pos1_world = configuration.get_transform_frame_to_world(
+            self.frame[0]
+        ).translation
+        pos2_world = configuration.get_transform_frame_to_world(
+            self.frame[1]
+        ).translation
 
-        pos1_jac = configuration.get_frame_jacobian(self.frame[0])
+        pos1_jac = configuration.get_frame_jacobian(self.frame[0])[:3]
         # Transform jacobian to world aligned frame
-        rotation1 = configuration.get_transform_frame_to_world(self.frame[0]).rotation
-        pos1_jac[:3] = rotation1 @ pos1_jac[:3]
-        pos1_jac = np.block([np.eye(3), -pin.skew(self.O1)]) @ pos1_jac
+        rotation1 = configuration.get_transform_frame_to_world(
+            self.frame[0]
+        ).rotation
+        pos1_jac = rotation1 @ pos1_jac
 
-        pos2_jac = configuration.get_frame_jacobian(self.frame[1])
+        pos2_jac = configuration.get_frame_jacobian(self.frame[1])[:3]
         # Transform jacobian to world aligned frame
-        rotation2 = configuration.get_transform_frame_to_world(self.frame[1]).rotation
-        pos2_jac[:3] = rotation2 @ pos2_jac[:3]
-        pos2_jac = np.block([np.eye(3), -pin.skew(self.O2)]) @ pos2_jac
+        rotation2 = configuration.get_transform_frame_to_world(
+            self.frame[1]
+        ).rotation
+        pos2_jac = rotation2 @ pos2_jac
 
-        dh_dx = 2 * np.concatenate([pos1_world - pos2_world, pos2_world - pos1_world])
+        dh_dx = 2 * np.concatenate(
+            [pos1_world - pos2_world, pos2_world - pos1_world]
+        )
         dx_dq = np.vstack([pos1_jac, pos2_jac])
         return dh_dx.T @ dx_dq
