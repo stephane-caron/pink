a82e1b0f4da0688f40b60b0bae020b32086cea43
Ivan Domrachev <domrachev03@mail.ru>
2024-05-18 19:52:29 +0300

    chore: copyright information in barriers init

--- pink/barriers/__init__.py
+++ pink/barriers/__init__.py
@@ -1,3 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# SPDX-License-Identifier: Apache-2.0
+# Copyright 2024 Stéphane Caron, Ivan Domrachev, Simeon Nedelchev
 """Control Barrier Functions."""
 
 from .barrier import Barrier
