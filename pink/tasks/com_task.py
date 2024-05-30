bdab5fac341e02f2abd24fff9b505831c84e5612
simeon <simkaned@gmail.com>
2024-05-30 23:28:49 +0300

    added docstrings for com task

--- pink/tasks/com_task.py
+++ pink/tasks/com_task.py
@@ -4,7 +4,7 @@
 # SPDX-License-Identifier: Apache-2.0
 # Copyright 2024 Stéphane Caron, Simeon Nedelchev, Ivan Domrachev
 
-"""Frame task implementation."""
+"""Center of mass task implementation."""
 
 from typing import Optional, Sequence, Union
 
@@ -15,30 +15,21 @@ from ..configuration import Configuration
 from .exceptions import TargetNotSet, TaskDefinitionError
 from .task import Task
 
-# TODO: Rewrite doc strings
 
 class ComTask(Task):
-    r"""Regulate the pose of a robot frame in the world frame.
+    r"""Regulate the center of mass of a robot.
 
     Attributes:
-        frame: Frame name, typically the name of a link or joint from the robot
-            description.
-        transform_target_to_world: Target pose of the frame.
+        target_com: Target position of the center of mass.
 
-    Costs are designed so that errors with varying SI units, here position and
-    orientation displacements, can be cast to homogeneous values. For example,
-    if task "foo" has a position cost of 1.0 and task "bar" a position cost of
-    0.1, then a 1 [cm] error in task "foo" costs as much as a 10 [cm] error in
-    task "bar".
-
-    Note:
-        Dimensionally, the 6D cost vector is a (normalized) force screw and our
-        objective function is a (normalized) energy.
+    Costs are designed so that errors with varying SI units, here position
+    displacements, can be cast to homogeneous values. For example, if task
+    "foo" has a position cost of 1.0 and task "bar" a position cost of 0.1,
+    then a 1 [cm] error in task "foo" costs as much as a 10 [cm] error in task
+    "bar".
     """
 
-    frame: str
-    target_com: np.ndarray
-
+    target_com: Optional[np.ndarray]
 
     def __init__(
         self,
@@ -46,19 +37,13 @@ class ComTask(Task):
         lm_damping: float = 0.0,
         gain: float = 1.0,
     ) -> None:
-        r"""Define a new frame task.
+        r"""Define a new center of mass task.
 
         Args:
-            frame: Frame name, typically the name of a link or joint from the
-                robot description.
-            position_cost: Contribution of position errors to the normalized
-                cost, in :math:`[\mathrm{cost}] / [\mathrm{m}]`. If this is a
-                vector, the cost is anisotropic and each coordinate corresponds
-                to an axis of the frame.
-            orientation_cost: Contribution of orientation errors to the
-                normalized cost, in :math:`[\mathrm{cost}] / [\mathrm{rad}]`.
-                If this is a vector, the cost is anisotropic and each
-                coordinate corresponds to an axis of the frame.
+            cost: Contribution of position errors to the normalized cost, in
+                :math:`[\mathrm{cost}] / [\mathrm{m}]`. If this is a vector,
+                the cost is anisotropic and each coordinate corresponds to an
+                axis of the frame.
             lm_damping: Levenberg-Marquardt damping (see class attributes). The
                 default value is conservatively low.
             gain: Task gain :math:`\alpha \in [0, 1]` for additional low-pass
@@ -70,10 +55,8 @@ class ComTask(Task):
             gain=gain,
             lm_damping=lm_damping,
         )
-
         self.lm_damping = lm_damping
         self.target_com = None
-        #
         self.set_cost(cost)
 
     def set_cost(
@@ -82,13 +65,14 @@ class ComTask(Task):
         r"""Set a new cost for all 3D CoM position coordinates.
 
         Args:
-            position_cost: Contribution of position errors to the normalized
-                cost, in :math:`[\mathrm{cost}] / [\mathrm{m}]`. If this is a
-                vector, the cost is anisotropic and each coordinate.
+            cost: Contribution of position errors to the normalized cost, in
+                :math:`[\mathrm{cost}] / [\mathrm{m}]`. If this is a vector,
+                the cost is anisotropic and each coordinate corresponds to an
+                axis of the frame.
         """
         if isinstance(cost, float):
             assert cost >= 0.0
-        else:  # not isinstance(position_cost, float)
+        else:  # not isinstance(cost, float)
             assert all(cost >= 0.0 for cost in cost)
         if isinstance(self.cost, np.ndarray):
             self.cost[0:3] = cost
@@ -98,88 +82,61 @@ class ComTask(Task):
                 f"currently cost={self.cost}"
             )
 
-
     def set_target(
         self,
         target_com: np.ndarray,
     ) -> None:
-        """Set task target pose in the world frame.
+        """Set the target CoM position in the world frame.
 
         Args:
-            transform_target_to_world: Transform from the task target frame to
-                the world frame.
+            target_com: Target position of the center of mass in the world
+                frame.
         """
         self.target_com = target_com.copy()
 
     def set_target_from_configuration(
         self, configuration: Configuration
     ) -> None:
-        """Set task target pose from a robot configuration.
+        """Set the task target pose from a given robot configuration.
 
         Args:
-            configuration: Robot configuration.
+            configuration: Robot configuration used to compute the target CoM.
         """
         q = configuration.q.copy()
         desired_com = pin.centerOfMass(configuration.model, configuration.data, q)
         self.set_target(desired_com)
 
-
     def compute_error(self, configuration: Configuration) -> np.ndarray:
-        r"""Compute frame task error.
+        r"""Compute the CoM task error.
 
-        This error is a twist :math:`e(q) \in se(3)` expressed in the local
-        frame (i.e. it is a *body* twist). We map it to :math:`\mathbb{R}^6`
-        using Pinocchio's convention where linear coordinates are followed by
-        angular coordinates.
-
-        The error is the right-minus difference between the target pose
-        :math:`T_{0t}` and current frame pose :math:`T_{0b}`:
-
-        .. math::
-
-            e(q) := {}_b \xi_{0b} = -(T_{t0} \boxminus T_{b0})
-            = -\log(T_{t0} \cdot T_{0b}) = -\log(T_{tb}) = \log(T_{bt})
-
-        where :math:`b` denotes our frame, :math:`t` the target frame and
-        :math:`0` the inertial frame.
-
-        See :func:`pink.tasks.task.Task.compute_error` for more context, and
-        [MLT]_ for details on the right-minus operator.
+        The error is the difference between the target CoM position and the
+        current CoM position, both expressed in the world frame.
 
         Args:
             configuration: Robot configuration :math:`q`.
 
         Returns:
-            Frame task error :math:`e(q)`.
+            CoM task error :math:`e(q)`.
         """
         if self.target_com is None:
             raise TargetNotSet(f"no target set for CoM")
-        
         q = configuration.q.copy()
-        error = pin.centerOfMass(configuration.model, configuration.data, q) - self.target_com
+        actual_com = pin.centerOfMass(configuration.model, configuration.data, q)
+        error = actual_com - self.target_com
         return error
 
     def compute_jacobian(self, configuration: Configuration) -> np.ndarray:
-        r"""Compute the frame task Jacobian.
+        r"""Compute the CoM task Jacobian.
 
-        The task Jacobian :math:`J(q) \in \mathbb{R}^{6 \times n_v}` is the
-        derivative of the task error :math:`e(q) \in \mathbb{R}^6` with respect
-        to the configuration :math:`q`. The formula for the frame task is:
-
-        .. math::
-
-            J(q) = -\text{Jlog}_6(T_{tb}) {}_b J_{0b}(q)
-
-        The derivation of the formula for this Jacobian is detailed in
-        [FrameTaskJacobian]_. See also
-        :func:`pink.tasks.task.Task.compute_jacobian` for more context on task
-        Jacobians.
+        The task Jacobian :math:`J(q) \in \mathbb{R}^{3 \times n_v}` is the
+        derivative of the CoM position with respect to the configuration
+        :math:`q`.
 
         Args:
             configuration: Robot configuration :math:`q`.
 
         Returns:
-            Jacobian matrix :math:`J`, expressed locally in the frame.
+            Jacobian matrix :math:`J`.
         """
         if self.target_com is None:
             raise TargetNotSet(f"no target set for CoM")
@@ -189,11 +146,12 @@ class ComTask(Task):
 
     def __repr__(self):
         """Human-readable representation of the task."""
-        position_cost = (
+        cost = (
             self.cost if isinstance(self.cost, float) else self.cost[0:3]
         )
         return (
             "ComTask("
             f"gain={self.gain}, "
-            f"com_cost={position_cost}"
+            f"position_cost={cost}, "
+            f"target_com={self.target_com})"
         )
