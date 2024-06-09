83503c9eecb08bf9587d1e895063218b789a082c
DomrachevIvan <domrachev03@mail.ru>
2024-06-10 00:51:21 +0300

    Update pink/barriers/body_spherical_barrier.py
    Co-authored-by: Stéphane Caron <stephane.caron@inria.fr>

--- pink/barriers/body_spherical_barrier.py
+++ pink/barriers/body_spherical_barrier.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 #
 # SPDX-License-Identifier: Apache-2.0
-# Copyright 2022 Stéphane Caron
+# Copyright 2024 Ivan Domrachev, Simeon Nedelchev
 
 """Barrier based on the distance between two frames."""
 
