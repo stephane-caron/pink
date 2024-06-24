81fbca0ca2e98d706d55101ea1a164ff4bd95df9
Ivan Domrachev <domrachev03@mail.ru>
2024-06-25 02:23:53 +0300

    docs: documantation of self_collision_barrier

--- pink/barriers/self_collision_barrier.py
+++ pink/barriers/self_collision_barrier.py
@@ -2,9 +2,9 @@
 # -*- coding: utf-8 -*-
 #
 # SPDX-License-Identifier: Apache-2.0
-# Copyright 2022 Stéphane Caron
+# Copyright 2022 Domrachev Ivan, Simeon Nedelchec
 
-"""General description"""
+"""Self Collision Avoidance Barrier with hpp-fcl"""
 
 from typing import Union
 
@@ -16,13 +16,27 @@ from .barrier import Barrier
 
 
 class SelfCollisionBarrier(Barrier):
-    r"""Abstract class description.
+    r"""Self Collision Avoidance Barrier.
+
+    This class defines a barrier function based on the smooth convex collision geometry.
+    using hpp-fcl through Pinocchio. Note that for non-smooth collision geometries
+    behaviour is undefined
+
+    The barrier function is defined as:
+
+    .. math::
+
+        h(q) = \begin{bmatrix} \ldots \\ d(p^1_i, p^2_i) - d_{min}^2 \\ \ldots \end{bmatrix}
+            \quad \forall i \in 0 \ldots N
+
+    where :math:`N` is number of collision pairs, :math:`p^k_i` is the :math:`k-th` body in :math:`i`-th collision pair,
+    :math:`d(p^1_i, p^2_i)` is the distance between collision bodies in the pair,
+    and :math:`d_{min}` is minimal distance between any collision bodies.
 
     Attributes:
-        ...
+        d_min: Minimum distance between collision pairs.
     """
 
-    frames: tuple[str, str]
     d_min: float
 
     def __init__(
@@ -32,7 +46,16 @@ class SelfCollisionBarrier(Barrier):
         safe_displacement_gain: float = 1.0,
         d_min: float = 0.02,
     ):
-        """..."""
+        """Initialization of the SelfCollisionBarrier.
+
+        Args:
+            n_collision_pairs: Number of collision pairs.
+            gain: Barrier gain. Defaults to 1.0.
+            safe_displacement_gain: gain for the safe backup displacement.
+                cost term. Defaults to 1.0.
+            d_min: Minimum distance between any collision pairs.
+                Defaults to 0.02.
+        """
         super().__init__(
             dim=n_collision_pairs,
             gain=gain,
@@ -43,17 +66,61 @@ class SelfCollisionBarrier(Barrier):
         self.__q_prev = None
 
     def compute_barrier(self, configuration: Configuration) -> np.ndarray:
-        """..."""
+        r"""Compute the value of the barrier function.
+
+        The barrier function is computed as distnace between all collision pairs.
+
+        The barrier function is defined as:
+
+        .. math::
+
+            h(q) = \begin{bmatrix} \ldots \\ d(p^1_i, p^2_i) - d_{min}^2 \\ \ldots \end{bmatrix}
+                \quad \forall i \in 0 \ldots N
+
+        where :math:`N` is number of collision pairs, :math:`p^k_i` is the :math:`k-th` body in :math:`i`-th collision pair,
+        :math:`d(p^1_i, p^2_i)` is the distance between collision bodies in the pair,
+        and :math:`d_{min}` is minimal distance between any collision bodies.
+
+        Args:
+            configuration: Robot configuration :math:`q`.
+
+        Returns:
+            Value of the barrier function
+                 :math:`h(q)`.
+        """
         return np.array(
             [
-                # configuration.collision_data.distanceResults[k].min_distance - self.d_min
-                configuration.collision_data.distanceResults[k].min_distance ** 2 - self.d_min**2
+                configuration.collision_data.distanceResults[k].min_distance - self.d_min
                 for k in range(len(configuration.collision_model.collisionPairs))
             ]
         )
 
     def compute_jacobian(self, configuration: Configuration) -> np.ndarray:
-        """..."""
+        r"""Compute the Jacobian matrix of the barrier function.
+
+        The jacobian matrix could be represented as stacked gradients of each
+        collision pair distance function w.r.t. joints. They are computed based on
+        frames jacobians and normal surface vector at nearest distance points n.
+
+        The gradient, a.k.a i-th row in the jacobian matrix, is given by:
+
+        .. math::
+
+            J_i = n_1^T J^1_p + (r_1 \times n_1)^T J^1_w + n_2^T J^2_p + (r_2 \times n_2)^T J^2_w,
+
+        where :math:`n_{1,2}` are normal vectors (note that :math:`n_1 = -n_2`), :math:`r_{1, 2}` are vectors from
+        frame origin and nearest point, :math:`J^{1, 2}_{p, w}` are position/orientation Jacobians of
+        respective frame.
+
+        Args:
+            configuration: Robot configuration :math:`q`.
+
+        Returns:
+            Jacobian matrix
+                :math:`\frac{\partial h}{\partial q}(q)`.
+        """
+
+        # Get robot and collision models and datas
         model = configuration.model
         data = configuration.data
         collision_model = configuration.collision_model
@@ -68,24 +135,33 @@ class SelfCollisionBarrier(Barrier):
             go_1 = collision_model.geometryObjects[cp.first]
             go_2 = collision_model.geometryObjects[cp.second]
 
-            f1_id = go_1.parentJoint
-            f2_id = go_2.parentJoint
+            f1_id = go_1.parentFrame
+            f2_id = go_2.parentFrame
 
             w1 = np.array(dr.getNearestPoint1())
             w2 = np.array(dr.getNearestPoint2())
 
-            r1 = np.array(w1 - data.oMi[f1_id].translation)
-            r2 = np.array(w2 - data.oMi[f2_id].translation)
-            # TODO: normal for some reason it not calculated
+            # Vectors from frame origin to nearest points
+            r1 = np.array(w1 - data.oMf[f1_id].translation)
+            r2 = np.array(w2 - data.oMf[f2_id].translation)
+
+            # Normal vector betwee nearest points (n_1 in the notation above)
             n = (w1 - w2) / np.linalg.norm(w1 - w2)
 
-            # Approach 1: from casadi_kin_dyn
-            J_1 = pin.getJointJacobian(model, data, f1_id, pin.ReferenceFrame.LOCAL_WORLD_ALIGNED)
+            # Calculate first two terms using first frame jacobian
+            J_1 = pin.getFrameJacobian(model, data, f1_id, pin.ReferenceFrame.LOCAL_WORLD_ALIGNED)
             Jrow_v = n.T @ J_1[:3, :] + (pin.skew(r1) @ n).T @ J_1[3:, :]
 
-            J_2 = pin.getJointJacobian(model, data, f2_id, pin.ReferenceFrame.LOCAL_WORLD_ALIGNED)
+            # Calculate second two terms using second frame jacobian
+            # Note that minus appears, since n_2 = -n_1
+            J_2 = pin.getFrameJacobian(model, data, f2_id, pin.ReferenceFrame.LOCAL_WORLD_ALIGNED)
             Jrow_v -= n.T @ J_2[:3, :] + (pin.skew(r2) @ n).T @ J_2[3:, :]
 
             J[k] = Jrow_v.copy()
+
+        # If collision is undefined, or during the collision, some values
+        # might be nans. In this case, set them to zero.
+        # Note that for non-colliding smooth convex functions no nans are present.
+        J = np.nan_to_num(J)
 
         return J
