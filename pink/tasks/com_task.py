3a6351384b094a1e1ffaa4fd3bd10cbf7effa698
Ivan Domrachev <domrachev03@mail.ru>
2024-06-26 23:47:55 +0300

    test: test com task

--- pink/tasks/com_task.py
+++ pink/tasks/com_task.py
@@ -33,7 +33,7 @@ class ComTask(Task):
 
     def __init__(
         self,
-        cost: Union[float, Sequence[float]],
+        cost: Union[float, Sequence[float], np.ndarray],
         lm_damping: float = 0.0,
         gain: float = 1.0,
     ) -> None:
@@ -55,7 +55,6 @@ class ComTask(Task):
             gain=gain,
             lm_damping=lm_damping,
         )
-        self.lm_damping = lm_damping
         self.target_com = None
         self.set_cost(cost)
 
@@ -70,17 +69,13 @@ class ComTask(Task):
                 the cost is anisotropic and each coordinate corresponds to an
                 axis of the frame.
         """
+
         if isinstance(cost, float):
             assert cost >= 0.0
         else:  # not isinstance(cost, float)
             assert all(cost >= 0.0 for cost in cost)
         if isinstance(self.cost, np.ndarray):
             self.cost[0:3] = cost
-        else:  # self.cost is not a vector
-            raise TaskDefinitionError(
-                "CoM task cost should be a vector, "
-                f"currently cost={self.cost}"
-            )
 
     def set_target(
         self,
@@ -126,7 +121,7 @@ class ComTask(Task):
         actual_com = pin.centerOfMass(
             configuration.model, configuration.data, q
         )
-        error = actual_com - self.target_com
+        error = self.target_com - actual_com
         return error
 
     def compute_jacobian(self, configuration: Configuration) -> np.ndarray:
