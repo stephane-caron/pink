d1e670013fa2062b6270860a3d4cf17d2dbcb693
Stéphane Caron <stephane.caron@inria.fr>
2025-04-21 12:24:35 +0200

    WIP: Move pink.tasks.exceptions to pink.exceptions

--- pink/tasks/__init__.py
+++ pink/tasks/__init__.py
@@ -8,7 +8,6 @@
 
 from .com_task import ComTask
 from .damping_task import DampingTask
-from .exceptions import TargetNotSet, TaskJacobianNotSet
 from .frame_task import FrameTask
 from .joint_coupling_task import JointCouplingTask
 from .linear_holonomic_task import LinearHolonomicTask
@@ -28,7 +27,5 @@ __all__ = [
     "PostureTask",
     "RelativeFrameTask",
     "RollingTask",
-    "TargetNotSet",
     "Task",
-    "TaskJacobianNotSet",
 ]
