5543f4f5c73a3a91346bdf23d3da8f0b45891a8a
Stéphane Caron <stephane.caron@inria.fr>
2025-11-26 10:29:03 +0100

    examples: Minor changes to IK UR10 example

--- examples/inverse_kinematics_ur10.py
+++ examples/inverse_kinematics_ur10.py
@@ -4,7 +4,12 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 # /// script
-# dependencies = ["daqp", "pin-pink", "qpsolvers", "robot_descriptions"]
+# dependencies = [
+#     "daqp",
+#     "pin-pink",
+#     "qpsolvers",
+#     "robot_descriptions",
+# ]
 # ///
 
 """Move the end-effector of a UR10 arm to a prescribed target."""
@@ -24,15 +29,16 @@ stop_thres = 1e-8
 
 if __name__ == "__main__":
     robot = load_robot_description("ur10_description")
+    model = robot.model
 
     # Frame details
-    joint_name = robot.model.names[-1]
-    parent_joint = robot.model.getJointId(joint_name)
-    parent_frame = robot.model.getFrameId(joint_name)
+    joint_name = model.names[-1]
+    parent_joint = model.getJointId(joint_name)
+    parent_frame = model.getFrameId(joint_name)
     placement = pin.SE3.Identity()
 
     FRAME_NAME = "ee_frame"
-    ee_frame = robot.model.addFrame(
+    ee_frame = model.addFrame(
         pin.Frame(
             FRAME_NAME,
             parent_joint,
@@ -41,25 +47,25 @@ if __name__ == "__main__":
             pin.FrameType.OP_FRAME,
         )
     )
-    robot.data = pin.Data(robot.model)
-    low = robot.model.lowerPositionLimit
-    high = robot.model.upperPositionLimit
-    robot.q0 = pin.neutral(robot.model)
+    robot.data = pin.Data(model)
+    low = model.lowerPositionLimit
+    high = model.upperPositionLimit
+    q_init = pin.neutral(model)
 
     # Task details
     np.random.seed(0)
     q_final = np.array(
         [
             np.random.uniform(low=low[i], high=high[i], size=(1,))[0]
-            for i in range(6)
+            for i in range(model.nq)
         ]
     )
-    pin.forwardKinematics(robot.model, robot.data, q_final)
+    pin.forwardKinematics(model, robot.data, q_final)
     target_pose = robot.data.oMi[parent_joint]
     ee_task = FrameTask(FRAME_NAME, [1.0, 1.0, 1.0], [1.0, 1.0, 1.0])
     ee_task.set_target(target_pose)
 
-    configuration = pink.Configuration(robot.model, robot.data, robot.q0)
+    configuration = pink.Configuration(model, robot.data, q_init)
     error_norm = np.linalg.norm(ee_task.compute_error(configuration))
     print(f"Starting from {error_norm = :.2}")
     print(f"Desired precision is error_norm < {stop_thres}")
@@ -77,9 +83,9 @@ if __name__ == "__main__":
                 else qpsolvers.available_solvers[0]
             ),
         )
-        q_out = pin.integrate(robot.model, configuration.q, dv * dt)
-        configuration = pink.Configuration(robot.model, robot.data, q_out)
-        pin.updateFramePlacements(robot.model, robot.data)
+        q_out = pin.integrate(model, configuration.q, dv * dt)
+        configuration = pink.Configuration(model, robot.data, q_out)
+        pin.updateFramePlacements(model, robot.data)
         error_norm = np.linalg.norm(ee_task.compute_error(configuration))
         nb_steps += 1
 
