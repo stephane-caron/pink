bfe17b280db132e365d460496388fc82fb3f85d1
Stéphane Caron <stephane.caron@normalesup.org>
2026-04-20 16:03:07 +0200

    Switch new example from MeshCat to Viser

--- examples/humanoid_jvrc_com.py
+++ examples/humanoid_jvrc_com.py
@@ -4,24 +4,24 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 # /// script
-# dependencies = ["daqp", "loop-rate-limiters", "meshcat", "pin-pink",
-# "qpsolvers", "robot_descriptions", "typing_extensions"]
+# dependencies = ["daqp", "loop-rate-limiters", "viser", "pin-pink",
+# "qpsolvers", "robot_descriptions", "trimesh"]
 # ///
 
 """JVRC-1 humanoid backbending by regulating its CoM position."""
 
-import meshcat.geometry as g
-import meshcat.transformations as tf
 import numpy as np
 import pinocchio as pin
 import qpsolvers
+import trimesh
+import viser
 from loop_rate_limiters import RateLimiter
 from robot_descriptions.loaders.pinocchio import load_robot_description
 
 import pink
 from pink import solve_ik
 from pink.tasks import ComTask, FrameTask
-from pink.visualization import start_meshcat_visualizer
+from pink.visualization import start_viser_visualizer
 
 if __name__ == "__main__":
     robot = load_robot_description(
@@ -29,18 +29,13 @@ if __name__ == "__main__":
     )
 
     # Initialize visualization
-    viz = start_meshcat_visualizer(robot)
+    viz = start_viser_visualizer(robot)
     viewer = viz.viewer
-    viewer["com_target"].set_object(
-        g.Sphere(0.03), g.MeshLambertMaterial(color=0x00FF00)
-    )
-    viewer["com"].set_object(
-        g.Sphere(0.03), g.MeshLambertMaterial(color=0xFF0000)
-    )
 
     configuration = pink.Configuration(robot.model, robot.data, robot.q0)
     viz.display(configuration.q)
 
+    # Initialize IK tasks
     left_ankle_task = FrameTask(
         "l_ankle", position_cost=1.0, orientation_cost=3.0
     )
@@ -48,28 +43,65 @@ if __name__ == "__main__":
         "r_ankle", position_cost=1.0, orientation_cost=3.0
     )
     com_task = ComTask(cost=2.0)
+    tasks = [com_task, left_ankle_task, right_ankle_task]
 
-    transform_l_ankle_target_to_init = pin.SE3(
-        np.eye(3), np.array([0.1, 0.0, 0.0])
-    )
-    transform_r_ankle_target_to_init = pin.SE3(
-        np.eye(3), np.array([-0.1, 0.0, 0.0])
-    )
+    # IK: Set ankle task targets
     left_ankle_task.set_target(
         configuration.get_transform_frame_to_world("l_ankle")
-        * transform_l_ankle_target_to_init
+        * pin.SE3(np.eye(3), np.array([0.1, 0.0, 0.0]))
     )
     right_ankle_task.set_target(
         configuration.get_transform_frame_to_world("r_ankle")
-        * transform_r_ankle_target_to_init
+        * pin.SE3(np.eye(3), np.array([-0.1, 0.0, 0.0]))
     )
 
+    # IK: Set CoM task target
     initial_com = pin.centerOfMass(robot.model, robot.data, configuration.q)
     desired_com = initial_com.copy()
     desired_com[2] += 0.05
     com_task.set_target(desired_com)
 
-    tasks = [com_task, left_ankle_task, right_ankle_task]
+    # Visualization: add current and target CoM handles
+    sphere = trimesh.creation.icosphere(radius=0.03)
+    com_target_handle = viewer.scene.add_mesh_simple(
+        "com_target",
+        sphere.vertices,
+        sphere.faces,
+        color=(0.0, 1.0, 0.0),  # green
+    )
+    com_target_handle.position = tuple(desired_com.tolist())
+    com_handle = viewer.scene.add_mesh_simple(
+        "com",
+        sphere.vertices,
+        sphere.faces,
+        color=(1.0, 0.0, 0.0),  # red
+    )
+    com_handle.position = tuple(initial_com.tolist())
+
+    # Visualization: add ground plane
+    left_ankle_target = left_ankle_task.transform_target_to_world.translation
+    viewer.scene.set_background_image(np.full((1, 1, 3), 220, dtype=np.uint8))
+    viewer.scene.add_grid(
+        "/grid",
+        width=2.0,
+        height=2.0,
+        position=(0.0, 0.0, left_ankle_target[2] - 0.11),
+        plane_opacity=0.5,
+    )
+
+    # Interactive handle to drag the CoM target
+    transform_handle = viewer.scene.add_transform_controls(
+        "/com_target_control",
+        position=tuple(desired_com.tolist()),
+        wxyz=(1.0, 0.0, 0.0, 0.0),
+        scale=0.2,
+    )
+
+    @transform_handle.on_update
+    def _(handle: viser.TransformControlsEvent) -> None:
+        new_target = np.array(handle.target.position)
+        com_task.set_target(new_target)
+        com_target_handle.position = handle.target.position
 
     # Select QP solver
     solver = qpsolvers.available_solvers[0]
@@ -92,6 +124,6 @@ if __name__ == "__main__":
         )
         configuration.integrate_inplace(velocity, dt)
         viz.display(configuration.q)
-        viewer["com"].set_transform(tf.translation_matrix(com))
+        com_handle.position = tuple(com.tolist())
 
         rate.sleep()
