1fa0486f44c3cdf07969eb4cc8171a250085b3d4
Stéphane Caron <stephane.caron@normalesup.org>
2026-04-20 11:40:18 +0200

    Update list of contributors

--- pink/__init__.py
+++ pink/__init__.py
@@ -4,6 +4,7 @@
 # SPDX-License-Identifier: Apache-2.0
 # Copyright 2022 Stéphane Caron
 # Copyright 2024 Inria
+# Copyright 2026 CNRS
 
 """Inverse kinematics for articulated robot models, based on Pinocchio."""
 
